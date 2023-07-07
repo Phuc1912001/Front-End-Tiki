b0dc99a369b2618194a0e1301ef5e39a010e60aa
Phuc1912001 <phucphuc1912001@gmail.com>
2023-07-07 07:16:41 +0700

    signup-login

---
+++ src/Redux/Feature/userSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    avatar: '',
+    access_token: '',
+    id: '',
+    isAdmin: false,
+    refreshToken: ''
+}
+
+export const userSlide = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        updateUser: (state, action) => {
+            const { name = '', email = '', access_token = '', address = '', phone = '', avatar = '', _id = '', isAdmin } = action.payload
+            state.name = name;
+            state.email = email;
+            state.address = address ? address : state.address;
+            state.phone = phone ? phone : state.phone;
+            state.avatar = avatar ? avatar : state.avatar;
+            state.id = _id ? _id : state.id
+            state.isAdmin = isAdmin ? isAdmin : state.isAdmin;
+            state.access_token = access_token
+        },
+        resetUser: (state) => {
+            state.name = '';
+            state.id = '';
+            state.email = '';
+            state.address = '';
+            state.phone = '';
+            state.avatar = '';
+            state.access_token = '';
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { updateUser, resetUser } = userSlide.actions
+
+export default userSlide.reducer
+
