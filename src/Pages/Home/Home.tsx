9addc1ac9dfcfdcb57ea3e11bd37ee666aee6b8f
Phuc1912001 <phucphuc1912001@gmail.com>
2023-07-09 07:48:49 +0700

    admin page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Home.scss";
 import { Carousel } from "antd";
 import { imageUrlsCarousel } from "../../Image/Image";
+import { StarFilled } from "@ant-design/icons";
+
+import * as productService from "../../services/productSevice";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProduct] = useState<any>([]);
+
+  const fetchProductAll = async () => {
+    setLoading(true);
+    const res = await productService.getAllProduct();
+    setProduct(res.data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchProductAll();
+  }, []);
+
+  console.log("products", products);
+
   return (
     <Container>
       <Row>
@@ -30,6 +49,39 @@ const Home = () => {
               />
             </Col>
           </Row>
+        </Col>
+        <Col md={3}>
+          <div className="product-wrapper p-2">
+            <img
+              src="https://salt.tikicdn.com/cache/280x280/ts/product/7f/a7/e6/84c2933a6526b4b161eb07f83ab463c5.jpg.webp"
+              alt=""
+              className="img-fluid"
+            />
+            <div>Áo thun siêu ccao cấp hahaha</div>
+            <div>
+              <div>
+                <span>5</span>
+                <StarFilled
+                  style={{ fontSize: "1rem", color: "rgb(253, 216, 54)" }}
+                />
+              </div>
+              <div> | đã bán 14</div>
+              <div>
+                60.000đ <span>39%</span>
+              </div>
+            </div>
+            <hr />
+            <div>giao sieu tốc</div>
+          </div>
+        </Col>
+        <Col md={3}>
+          <div className="product-wrapper p-2">2</div>
+        </Col>
+        <Col md={3}>
+          <div className="product-wrapper p-2">2</div>
+        </Col>
+        <Col md={3}>
+          <div className="product-wrapper p-2">2</div>
         </Col>
       </Row>
     </Container>
